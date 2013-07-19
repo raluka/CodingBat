ca05d16510579cfa945041e61342ae9d66c1f5ef
raluka <badoi.raluca@gmail.com>
2013-07-19 21:04:53 +0200

    CodingBat  Logic -1

---
+++ src/Logic1/TestLogic1.java
@@ -0,0 +1,122 @@
+package Logic1;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: raluca.garba
+ * Date: 7/14/13
+ * Time: 4:02 AM
+ */
+public class TestLogic1 {
+    public static void main(String[] args) {
+        Logic1 logic1 = new Logic1();
+
+        System.out.println(logic1.cigarParty(30, false));
+        System.out.println(logic1.cigarParty(50, false));
+        System.out.println(logic1.cigarParty(70, true));
+        System.out.println(logic1.dateFashion(5, 10));
+        System.out.println(logic1.dateFashion(5, 2));
+        System.out.println(logic1.dateFashion(5, 5));
+        System.out.println(logic1.squirrelPlay(70, false));
+        System.out.println(logic1.squirrelPlay(95, false));
+        System.out.println(logic1.squirrelPlay(95, true));
+        System.out.println(logic1.caughtSpeeding(60, false));
+        System.out.println(logic1.caughtSpeeding(65, false));
+        System.out.println(logic1.caughtSpeeding(65, true));
+        System.out.println(logic1.sortaSum(3, 4));
+        System.out.println(logic1.sortaSum(9, 4));
+        System.out.println(logic1.sortaSum(10, 11));
+        System.out.println(logic1.alarmClock(1, false));
+        System.out.println(logic1.alarmClock(5, false));
+        System.out.println(logic1.alarmClock(0, false));
+        System.out.println(logic1.love6(6, 4));
+        System.out.println(logic1.love6(4, 5));
+        System.out.println(logic1.love6(1, 5));
+        System.out.println(logic1.in1To10(5, false));
+        System.out.println(logic1.in1To10(11, false));
+        System.out.println(logic1.in1To10(11, true));
+        System.out.println(logic1.specialEleven(22));
+        System.out.println(logic1.specialEleven(23));
+        System.out.println(logic1.specialEleven(24));
+        System.out.println(logic1.more20(20));
+        System.out.println(logic1.more20(21));
+        System.out.println(logic1.more20(22));
+        System.out.println(logic1.old35(3));
+        System.out.println(logic1.old35(10));
+        System.out.println(logic1.old35(15));
+        System.out.println(logic1.less20(18));
+        System.out.println(logic1.less20(19));
+        System.out.println(logic1.less20(20));
+        System.out.println(logic1.nearTen(12));
+        System.out.println(logic1.nearTen(17));
+        System.out.println(logic1.nearTen(19));
+        System.out.println(logic1.teenSum(3, 4));
+        System.out.println(logic1.teenSum(10, 13));
+        System.out.println(logic1.teenSum(13, 2));
+        System.out.println(logic1.answerCell(false, false, false));
+        System.out.println(logic1.answerCell(false, false, true));
+        System.out.println(logic1.answerCell(true, false, false));
+        System.out.println(logic1.teaParty(6, 8));
+        System.out.println(logic1.teaParty(3, 8));
+        System.out.println(logic1.teaParty(20, 6));
+        System.out.println(logic1.fizzString("fig"));
+        System.out.println(logic1.fizzString("dib"));
+        System.out.println(logic1.fizzString("fib"));
+        System.out.println(logic1.fizzString2(1));
+        System.out.println(logic1.fizzString2(2));
+        System.out.println(logic1.fizzString2(3));
+        System.out.println(logic1.twoAsOne(1, 2, 3));
+        System.out.println(logic1.twoAsOne(3, 1, 2));
+        System.out.println(logic1.twoAsOne(3, 2, 2));
+        System.out.println(logic1.inOrder(1, 2, 4, false));
+        System.out.println(logic1.inOrder(1, 2, 1, false));
+        System.out.println(logic1.inOrder(1, 1, 2, true));
+        System.out.println(logic1.inOrderEqual(2, 5, 11, false));
+        System.out.println(logic1.inOrderEqual(5, 7, 6, false));
+        System.out.println(logic1.inOrderEqual(5, 5, 7, true));
+        System.out.println(logic1.lastDigit(23, 19, 13));
+        System.out.println(logic1.lastDigit(23, 19, 12));
+        System.out.println(logic1.lastDigit(23, 19, 3));
+        System.out.println(logic1.lessBy10(1, 7, 11));
+        System.out.println(logic1.lessBy10(1, 7, 10));
+        System.out.println(logic1.lessBy10(11, 1, 7));
+        System.out.println(logic1.withoutDoubles(2, 3, true));
+        System.out.println(logic1.withoutDoubles(3, 3, true));
+        System.out.println(logic1.withoutDoubles(3, 3, false));
+        System.out.println(logic1.maxMod5(2, 3));
+        System.out.println(logic1.maxMod5(6, 2));
+        System.out.println(logic1.maxMod5(3, 2));
+        System.out.println(logic1.redTicket(2, 2, 2));
+        System.out.println(logic1.redTicket(2, 2, 1));
+        System.out.println(logic1.redTicket(0, 0, 0));
+        System.out.println(logic1.greenTicket(1, 2, 3));
+        System.out.println(logic1.greenTicket(2, 2, 2));
+        System.out.println(logic1.greenTicket(1, 1, 2));
+        System.out.println(logic1.blueTicket(9, 1, 0));
+        System.out.println(logic1.blueTicket(9, 2, 0));
+        System.out.println(logic1.blueTicket(6, 1, 4));
+        System.out.println(logic1.shareDigit(12, 23));
+        System.out.println(logic1.shareDigit(12, 43));
+        System.out.println(logic1.shareDigit(12, 44));
+        System.out.println(logic1.sumLimit(2, 3));
+        System.out.println(logic1.sumLimit(8, 3));
+        System.out.println(logic1.sumLimit(8, 1));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
